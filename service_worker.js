f6de8a2c3efe8f1ca140889bc98971209450e933
andieennn <andienswesty@gmail.com>
2025-07-09 20:37:12 +0700

    Add files via upload

---
+++ service_worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = ["index.html", "icon-192.png", "icon-512.png"];
+
+// Install service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch cache saat offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
